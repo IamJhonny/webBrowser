bbbe0dad150c11cc52b58b41b1c1fff15cc2b6e2
JHONNY <jhonnymanuel2001@gmail.com>
2023-06-08 15:35:28 -0400

    first commit webBrowser

---
+++ Jhonny Browser/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace Jhonny_Browser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.atras = new System.Windows.Forms.ToolStripButton();
+            this.adelante = new System.Windows.Forms.ToolStripButton();
+            this.refresh = new System.Windows.Forms.ToolStripButton();
+            this.stop = new System.Windows.Forms.ToolStripButton();
+            this.URL = new System.Windows.Forms.ToolStripTextBox();
+            this.go = new System.Windows.Forms.ToolStripButton();
+            this.home = new System.Windows.Forms.ToolStripButton();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.atras,
+            this.adelante,
+            this.refresh,
+            this.stop,
+            this.URL,
+            this.go,
+            this.home});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip1.Size = new System.Drawing.Size(693, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // atras
+            // 
+            this.atras.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.atras.Image = ((System.Drawing.Image)(resources.GetObject("atras.Image")));
+            this.atras.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.atras.Name = "atras";
+            this.atras.Size = new System.Drawing.Size(23, 22);
+            this.atras.Text = "atras";
+            this.atras.Click += new System.EventHandler(this.atras_Click);
+            // 
+            // adelante
+            // 
+            this.adelante.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.adelante.Image = ((System.Drawing.Image)(resources.GetObject("adelante.Image")));
+            this.adelante.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.adelante.Name = "adelante";
+            this.adelante.Size = new System.Drawing.Size(23, 22);
+            this.adelante.Text = "adelante";
+            this.adelante.Click += new System.EventHandler(this.adelante_Click);
+            // 
+            // refresh
+            // 
+            this.refresh.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.refresh.Image = ((System.Drawing.Image)(resources.GetObject("refresh.Image")));
+            this.refresh.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(23, 22);
+            this.refresh.Text = "refresh";
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // stop
+            // 
+            this.stop.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.stop.Image = ((System.Drawing.Image)(resources.GetObject("stop.Image")));
+            this.stop.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(23, 22);
+            this.stop.Text = "toolStripButton2";
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // URL
+            // 
+            this.URL.BackColor = System.Drawing.Color.Silver;
+            this.URL.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.URL.Name = "URL";
+            this.URL.Size = new System.Drawing.Size(400, 25);
+            // 
+            // go
+            // 
+            this.go.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.go.Image = ((System.Drawing.Image)(resources.GetObject("go.Image")));
+            this.go.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.go.Name = "go";
+            this.go.Size = new System.Drawing.Size(23, 22);
+            this.go.Text = "toolStripButton4";
+            this.go.Click += new System.EventHandler(this.go_Click);
+            // 
+            // home
+            // 
+            this.home.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.home.Image = ((System.Drawing.Image)(resources.GetObject("home.Image")));
+            this.home.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.home.Name = "home";
+            this.home.Size = new System.Drawing.Size(23, 22);
+            this.home.Text = "toolStripButton5";
+            this.home.Click += new System.EventHandler(this.home_Click);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 25);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.ScriptErrorsSuppressed = true;
+            this.webBrowser.Size = new System.Drawing.Size(693, 425);
+            this.webBrowser.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(693, 450);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JhonnyBrowser";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton atras;
+        private System.Windows.Forms.ToolStripButton adelante;
+        private System.Windows.Forms.ToolStripButton refresh;
+        private System.Windows.Forms.ToolStripButton stop;
+        private System.Windows.Forms.ToolStripTextBox URL;
+        private System.Windows.Forms.ToolStripButton go;
+        private System.Windows.Forms.ToolStripButton home;
+        private System.Windows.Forms.WebBrowser webBrowser;
+    }
+}
+
---
+++ Jhonny Browser/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jhonny_Browser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void go_Click(object sender, EventArgs e)
+        {
+            webBrowser.Navigate(URL.Text);
+        }
+
+        private void atras_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoBack();
+        }
+
+        private void adelante_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoForward();
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            webBrowser.Refresh();
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            webBrowser.Stop();
+        }
+
+        private void home_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoHome();
+        }
+    }
+}
